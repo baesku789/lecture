a4e7635abb722aa5137cd9caa8117b7c0b1d961f
baesku789 <baesku789@gmail.com>
2021-06-17 11:07:33 +0900

    format server_tcp.js

--- lecture/node/server_tcp.js
+++ lecture/node/server_tcp.js
@@ -20,14 +20,14 @@ server.listen(8107, function() {
     });
     server.on('error', function(err){
     });
-    });
-    function writeData(socket, data){
+});
+function writeData(socket, data){
     var success = !socket.write(data);
     if (!success){
-    (function(socket, data){
-    socket.once('drain', function(){
-    writeData(socket, data);
-    });
-    })(socket, data);
+        (function(socket, data){
+            socket.once('drain', function(){
+                writeData(socket, data);
+            });
+        })(socket, data);
     }
-    }
+}
